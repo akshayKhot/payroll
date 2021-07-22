94ec4bd08f1f92539ec729bcb39c1e8173407d47
Akshay Khot <akshay.khot@hey.com>
2021-07-22 18:20:26 +0530

    persist uploaded data

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -1,7 +1,21 @@
 class PayrollController < ApplicationController
+
   def upload
+    puts "File was uploaded"
+  end
+
+  def create
+    employee_report = params[:employee_report]
+    data = File.read(employee_report)
+    TimeReport.parse(data)
+
+    redirect_to :controller => "payroll", :action => "success"
   end
 
   def report
+  end
+
+  def success
+
   end
 end
--- app/models/time_report.rb
+++ app/models/time_report.rb
@@ -1,2 +1,13 @@
+require "csv"
+
 class TimeReport < ApplicationRecord
+
+  def self.parse(data)
+    time_reports = []
+    CSV.parse(data, headers: true).each do |row|
+      time_reports << { date: row["date"], hours_worked: row["hours worked"], employee_id: row["employee id"], job_group: row["job group"], created_at: Time.now, updated_at: Time.now }
+    end
+
+    TimeReport.insert_all(time_reports)
+  end
 end
