a8b56f036ebaed83b76818046e19ba06c8b88f7e
Akshay Khot <akshay.khot@hey.com>
2021-07-24 21:27:15 +0530

    remove instance variable

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,10 +1,8 @@
 
 class Report
-  def initialize
-    @employee_reports = []
-  end
 
   def generate
+    employee_reports = []
 
     # Tracks each employees daily work hours
     employee_daily_work_hours = {}
@@ -20,19 +18,19 @@ class Report
     employee_daily_work_hours.each do |employee, dail_work_hours|
       dail_work_hours.each do |daily_hours|
         pay_period = PayPeriod.new(daily_hours.start_date, daily_hours.end_date)
-        emp_report = @employee_reports.find { |er| er.employee_id == employee.id && er.pay_period.start_date == pay_period.start_date && er.pay_period.end_date == pay_period.end_date }
+        emp_report = employee_reports.find { |er| er.employee_id == employee.id && er.pay_period.start_date == pay_period.start_date && er.pay_period.end_date == pay_period.end_date }
 
         if emp_report
           emp_report.amount_paid += employee.amount_paid(daily_hours.hours)
         else
-          @employee_reports << EmployeeReport.new(employee.id, pay_period, employee.amount_paid(daily_hours.hours))
+          employee_reports << EmployeeReport.new(employee.id, pay_period, employee.amount_paid(daily_hours.hours))
         end
       end
     end
 
     {
       payroll_report: {
-        employee_reports: @employee_reports
+        employee_reports: employee_reports
       }
     }
   end
