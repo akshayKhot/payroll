b64f90a1a6574f561e9a953baaa2706a4f87cec6
Akshay Khot <akshay.khot@hey.com>
2021-07-24 21:34:58 +0530

    Move behavior

--- app/models/employee_report.rb
+++ app/models/employee_report.rb
@@ -8,4 +8,10 @@ class EmployeeReport
     @pay_period = pay_period
     @amount_paid = amount_paid
   end
+
+  def belongs_to_employee_for_pay_period?(employee_id, pay_period)
+    @employee_id == employee_id &&
+    @pay_period.start_date == pay_period.start_date &&
+    @pay_period.end_date == pay_period.end_date
+  end
 end
