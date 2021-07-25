52d83432fcab0a67abd382409a5cc3f792d25ca8
Akshay Khot <akshay.khot@hey.com>
2021-07-25 19:15:38 +0530

    mvc to api

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -2,31 +2,22 @@ class PayrollController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def upload
-  end
-
-  def create
     employee_report = params[:employee_report]
     file_num = File.basename(employee_report.original_filename, ".csv").split("-").last
 
     if TimeReport.exists?(file_number: file_num)
-      redirect_to :controller => "payroll", :action => "duplicate"
+      render json: {status: "error", message: "You've already uploaded a file with the same report ID."}
       return
     end
 
     data = File.read(employee_report)
     TimeReport.parse(file_num, data)
 
-    redirect_to :controller => "payroll", :action => "success"
+    render json: {status: "success", message: "The file is successfully uploaded."}
   end
 
   def report
     render :json => Report.new.generate
-  end
-
-  def success
-  end
-
-  def duplicate
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,4 @@
 Rails.application.routes.draw do
-  get 'payroll/upload'
-  get 'payroll/report'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get "/api/upload" => "payroll#upload"
-  post "/payroll/create" => "payroll#create"
-
-  get "/api/report" => "payroll#report"
-  get "/success" => "payroll#success"
-  get "/duplicate" => "payroll#duplicate"
+  post "/api/payroll/upload" => "payroll#upload"
+  get "/api/payroll/report" => "payroll#report"
 end
