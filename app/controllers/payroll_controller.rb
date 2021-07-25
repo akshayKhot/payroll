9cc01fd1a1263e1f20cb48192dc432aa22db25b4
Akshay Khot <akshay.khot@hey.com>
2021-07-25 19:03:29 +0530

    enable postman requests

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -1,4 +1,5 @@
 class PayrollController < ApplicationController
+  skip_before_action :verify_authenticity_token
 
   def upload
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'payroll/upload'
   get 'payroll/report'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/api/upload" => "payroll#upload"
   post "/payroll/create" => "payroll#create"
