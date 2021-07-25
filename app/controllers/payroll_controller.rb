3ff20cef0a58e8b8ee41cddc0ac6e26f8759d598
Akshay Khot <akshay.khot@hey.com>
2021-07-25 19:23:18 +0530

    rename file

--- app/controllers/payroll_controller.rb
+++ app/controllers/payroll_controller.rb
@@ -2,15 +2,15 @@ class PayrollController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def upload
-    employee_report = params[:employee_report]
-    file_num = File.basename(employee_report.original_filename, ".csv").split("-").last
+    employee_report_file = params[:employee_report]
+    file_num = File.basename(employee_report_file.original_filename, ".csv").split("-").last
 
     if TimeReport.exists?(file_number: file_num)
       render json: {status: "error", message: "You've already uploaded a file with the same report ID."}
       return
     end
 
-    data = File.read(employee_report)
+    data = File.read(employee_report_file)
     TimeReport.parse(file_num, data)
 
     render json: {status: "success", message: "The file is successfully uploaded."}
